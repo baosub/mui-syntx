e2b0f402f5b88dabc4d9a64021d8d0c04ac54b74
Baldo <barrosoub@protonmail.com>
2024-05-29 13:50:22 -0600

    A navigation bar and two pages were added

---
+++ src/app/first/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1>First Page</h1>
+      
+    </>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavigationBar from './components/NavigationBar'
 
 const Home = () => {
   return (
     <>
+    <h1>Home
+
+    </h1>
+    <NavigationBar/>
     <h2>Hello, this is a nextjs-mui template</h2>
       
     </>

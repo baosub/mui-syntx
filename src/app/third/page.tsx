5647e4067d1dbbc5d5be4b86e1e93a4075fd99c1
Baldo <barrosoub@protonmail.com>
2024-06-06 20:30:57 -0600

    a menu was added

--- src/app/third/page.tsx
+++ src/app/third/page.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import { Container, Typography } from '@mui/material'
 import React from 'react'
 import ResponsiveNav from './responsivebar/ResponsiveNav'
 import ResponsiveBox from './responsivebar/ResponsiveBox'
+import Amenu from './responsivebar/Amenu'
 
 const page = () => {
   return (
@@ -9,6 +12,7 @@ const page = () => {
     <Typography textAlign='center' variant='h2' fontWeight={1000}>Responsive design with sx property</Typography>
     <ResponsiveNav/>
     <ResponsiveBox/>
+    <Amenu/>
       
     </Container>
   )
